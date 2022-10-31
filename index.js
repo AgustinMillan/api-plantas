5b8204dce282a6ab9d8b870f55c79214d9c4ad17
AgustinMillan <victor.agustin.millan@gmail.com>
2022-10-31 20:48:24 -0300

    f

---
+++ index.js
@@ -0,0 +1,128 @@
+let salidas = [
+  {
+    nombre: "Acacia cavens",
+    desc: "El acacia cavens, comúnmente llamado espinillo o aromo, es un arbusto que puede llegar a medir hasta 6 metros de alto. Se cultiva como especie ornamental y como cerco defensivo. Sus flores se utilizan como alimento de las abejas para producir miel; también son utilizadas sus maderas para hacer combustible, carbón vegetal.",
+    sustrato:
+      "Prefiere sustratos ricos en materia orgánica, arcillosos y bien drenados.",
+    floracion:
+      "Florece en primavera. Posee inflorescencias globosas, abundantes de color amarillo brillante y perfumadas.",
+    size: "Puede llegar a medir hasta 6 metros de alto.",
+    observaciones:
+      "Debido a su buen aroma, en la industria cosmética se usa la acacia caven para crear perfumes. Por esto, es muy común su cultivo en jardines, para que adquirirán un buen aroma.",
+    precio: "$2.892",
+    ubi: " Exterior. Pleno sol.",
+  },
+  {
+    nombre: "Ibira-pita",
+    desc: "Cada año este increíble árbol (Peltophorum dubium) va haciéndose más popular. Una de las razones es las bellas flores amarillas que cubren su copa. Otra, su fácil crecimiento y adaptación tanto a suelos arenosos como a aquellos un poco más arcillosos como los de Buenos Aires.\nEl Ibira-pita es un árbol caducifolio, es decir que pierde sus hojas cada año, es de aspecto fuerte, se desarrolla en su mayoría en lugares selváticos y bosques casi siempre se encuentra cerca de ríos, esto se debe a que siempre anda en busca de humedad, es de gran tamaño.",
+    sustrato: "Prefiere los suelos sueltos y bien drenados",
+    floracion:
+      "Florece en verano y principios de otoño; y fructifica a mediados de otoño.",
+    observaciones:
+      "Es recomendable plantarlo en la primavera para permitirle establecerse durante los meses de calor.",
+    ubi: "Exterior. Pleno sol.",
+  },
+  {
+    nombre: "Jazmín del Paraguay",
+    desc: "El jazmín del Paraguay (Brunfelsia Pauciflora), jazmín paraguayo o azuzena de paraguay; es una especie de arbusto perteneciente de la familia Solanaceae, del género Brunfelsia. Esta planta crece en distintos lugares del continente americano, principalmente en América del sur.\nTiene la singularidad de una vez finalizado su proceso de florecimiento, cambiar de tonalidades las flores, estas pasan de colores violetas oscuros en degrade hasta verse en tonalidades blancas.",
+    sustrato: "No es muy exigente. Sólo requiere estar bien drenado.",
+    floracion:
+      "Primavera-verano. Debido a la oxidación de sus flores, sucede ese efecto maravilloso de degradado. Primero comienzan con un violeta, luego a un azul claro y finalmente se transforman en flores blancas. Estas flores generan una fragancia que para algunos es parecida a un perfume dulce.",
+    size: "Alcanza entre 2 y 4 metros de altura.",
+    observaciones:
+      "Si tienes mascotas curiosas como perros o gatos, debes tener cuidado, porque sus raíces transmiten diferentes tóxicos al resto del planta y pueden provocar efectos letales.",
+    ubi: "Exterior, media sombra. Puede colocarse en pleno sol, pero puede suceder que se quemen sus hojas.",
+  },
+  {
+    nombre: "Pezuña de vaca",
+    desc: "El árbol pezuña de vaca (Bauhinia candicans) es de la familia de las fabáceas y su nombre proviene de la forma de sus hojas, semejantes a la huella de una pezuña.\nTiene importantes usos por su madera, conocida como falsa caoba, pero también por sus propiedades medicinales.",
+    sustrato: "Prefiere un suelo drenado ligero y fértil.",
+    observaciones:
+      "Sus raíces no son invasivas, pero necesita algo de espacio para poder desarrollarse correctamente.",
+    ubi: " Exterior. Pleno sol.",
+  },
+  {
+    nombre: "Anacahuita",
+    desc: "El anacahuita (Blepharocalyx Salicifolius) es un árbol de tronco oscuro, grueso y corteza algo agrietada, con follaje persistenta de color verde brillante. Es popularmente conocido como arrayán, Se puede usar en jardines, veredas. Es un árbol con buenas características para arbolado y jardines urbanos. Crece derecho, También puede plantarse en macetas.",
+    sustrato: "Prefiere suelos ricos, húmedos y sueltos.",
+    size: "Entre 3 y 6 metros, aunque en ambientes selváticos puede llegar a medir 20 metros.",
+    observaciones: "No soporta heladas intensas, ni prolongadas.",
+    ubi: " Exterior. Pleno sol.",
+  },
+  {
+    nombre: "Cortadera",
+    desc: "Se conoce con el nombre de gramíneas ornamentales a aquellas especies pertenecientes a dicha familia botánica, que poseen una parte aérea muy vistosa debido a sus colores, formas y tamaños, lo que las convierte en una interesante alternativa a la hora de adornar un jardín.\nEn este grupo se encuentra la cortadera o hierba de las Pampas (Cortaderia selloana) una planta herbácea perenne de pastos rizomatosos muy altos, endémica de la región pampeana argentina y en la Patagonia. También se conoce con los nombres comunes de plumerillo, zorro de la Pampa, ginerio o plumero. Esta es una planta utilizada principalmente en el ámbito ornamental, aunque también hay lugares donde son usadas para arreglos florales.",
+    sustrato:
+      "Prefiere sustratos fértiles, aireados y bien drenados. Para esto puede agregarse turba, pometina y arena gruesa.",
+    floracion:
+      "Tienden a florecer en verano y llegan a medir hasta unos 70 cm de largo. La paleta de colores de sus flores se ubica entre el blanco y el rosado fuerte.",
+    size: "Es una planta crece en densa masa de mata, con hojas perennes, estrechas y de bordes filosos, que pueden crecer hasta alcanzar los 2 o 3 metros de largo por 1 centímetro de ancho que se arquean hacia el suelo, de color verde azulado o gris plateado.",
+    observaciones:
+      "Tener en cuenta que la planta tiende a ser muy invasiva y su velocidad de crecimiento es bastante elevado.",
+    ubi: "Exterior. Pleno sol o media sombra.",
+  },
+  {
+    nombre: "Salvia guaranítica",
+    desc: "El género Salvia, familia de las Lamiaceae, lo forman unas 1.000 especies de arbustos y plantas herbáceas originarios de Asia, el Mediterráneo y del continente americano. La salvia guaranítica recibe los nombres vulgares de Salvia azul y Salvia guaraní. Se usan para formar borduras y grupos arbustivos siendo muy indicadas para zonas del jardín de bajo mantenimiento. También son aptas para macetas y jardineras.",
+    sustrato:
+      "Prefiere sustratos ricos en materia orgánica, arcillosos y bien drenados.",
+    floracion:
+      "Finales de verano hasta inicio de inverno. Las vistosas flores aparecen en espigas de unos 20 cm de altura y pueden ser de distintos tonos de azul según la variedad.",
+    size: "Puede alcanzar los 2 metros de altura.",
+    observaciones:
+      "Conviene ir eliminando las flores marchitas para favorecer la aparición de nuevas espigas. También se pueden podar ligeramente a finales del invierno para darles un aspecto más compacto.",
+    ubi: "Exterior. Pleno sol o media sombra.",
+  },
+  {
+    nombre: "Doxantha",
+    sustrato: "Requiere suelos fértiles y bien drenados.",
+    floracion:
+      "La floración comienza finales de primavera e inicios de verano. Las flores son amarillas, en forma de trompeta, de 4 a 5 cm de diámetro.",
+    size: "Puede alcanzar una altura de 12 metros.",
+    observaciones: ".",
+    ubi: "Exterior a pleno sol. Puede adaptarse a media sombra.",
+  },
+  {
+    nombre: "Lima tahiti",
+    sustrato: "Requiere suelos fértiles, profundos y bien drenados.",
+    floracion:
+      "La floración comienza antes de la primavera y dura de 15 a 30 días.",
+    size: "En tierra tiene un crecimiento normal, pueden adquirir grandes alturas, de entre 3 y 4 metros.",
+    observaciones:
+      "Es importante protegerlo del frío, ya que el mismo puede afectar su floración y por lo tanto la aparición de los frutos.",
+    ubi: " Exterior a pleno sol.",
+  },
+  {
+    nombre: "Rhus typhina",
+    desc: "Las hojas son compuestas, estando conformadas por entre 9 y 31 folíolos de hasta 55cm de largo. Normalmente el color de sus hojas es verde intenso, excepto en otoño, cuando se vuelven rojizas antes de caer. El Rhus typhina posee hojas compuestas, estas se encuentran conformadas por entre 9 y 31 folíolos de hasta 55cm de largo. Normalmente el color de sus hojas es verde intenso, excepto en otoño, cuando se vuelven rojizas antes de caer.",
+    sustrato:
+      "Necesita suelos profundos, drenados y ricos en materia orgánica.",
+    floracion:
+      "El árbol macho posee inflorescencias rosadas y largas, mientras que el hembra, aunque también posee inflorescencias rosadas estas son más acortadas y menos vistosas.",
+    size: "Alcanza una altura entre 3 y 10 metros.",
+    observaciones:
+      "Este árbol no posee raíces muy invasivas, por lo tanto, puede colocarse cerca de paredes o en las veredas.",
+    ubi: " Exterior, prefiere a pleno sol, aunque también se adapta a condiciones de media sombra.",
+  },
+  {
+    nombre: "Salvia procurrens",
+    desc: "El género Salvia, familia de las Lamiaceae, lo forman unas 1.000 especies de arbustos y plantas herbáceas originarios de Asia, el Mediterráneo y del continente americano. La salvia procurrens o salvia hederaceae es llamada vulgarmente yerba terrestre es un arbusto aromático perennifolio. Vive a orillas de arroyos, lagunas y otros lugares húmedos y es ideal como cubre suelos.",
+    sustrato:
+      "Prefiere sustratos ricos en materia orgánica, arcillosos y bien drenados.",
+    floracion:
+      "Entre otoño y principio de invierno; y puede volver a hacerlo desde finales de primavera hasta comienzos de verano. Las flores pequeñas emergen desde la base.",
+    observaciones:
+      "En algunas regiones del país se prepara un té con sus hojas mezcladas con hinojo y canela para aliviar la tos.",
+    ubi: "Exterior. Pleno sol o media sombra.",
+  },
+  {
+    nombre: "Solanum amygdalifolium",
+    sustrato: "Prefiere suelos bien drenados.",
+    floracion:
+      "Fines de primavera hasta fines de verano. Sus flores son perfumadas, en racimos de flores blancas o lilas, con centro amarillo.",
+    size: "Alcanza de 1 a 3 metros de altura.",
+    observaciones:
+      "Atrae gran cantidad de polinizadores y mariposas.\nControlar si se planta en suelo ya que es bastante invasora. Necesita apoyo para trepar.",
+    ubi: "Exterior. Pleno sol o media sombra.",
+  },
+];
